d28e44821e8d852ce7d5ac6553ab26581e1f7c22
fijaniques <fillipe.janiques@gmail.com>
2022-04-03 17:18:20 -0300

    stages connected

--- Door/Door.gd
+++ Door/Door.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
 func _on_Door_body_entered(body):
-	$Door.play()
-	$AnimationPlayer.play("Next")
 	get_tree().current_scene.get_node("Character").visible = false
+	_change_scene()
 
 
 func _change_scene():
